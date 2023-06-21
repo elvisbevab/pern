aaaa67f996b65efff7b663e781d779286dcf7f1c
elvisbevab <elvis.bevab@mailbox.org>
2023-06-21 11:20:32 +0200

    books routes responding to get requests

--- controllers/books.js
+++ controllers/books.js
@@ -1,17 +1,31 @@
 import { pool } from '../db.js';
 
+/**
+ * Return all books of the database
+ * @param {*} req
+ * @param {*} res
+ */
 export const getAllBooks = async (req, res) => {
   try {
     console.log('get All Books');
+    res.send('here are all books');
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
+    res.status(500).send('something went wrong');
   }
 };
 
+/**
+ * returns the one book with the id, given in the params
+ * @param {*} req
+ * @param {*} res
+ */
 export const getBook = async (req, res) => {
   try {
     console.log('getBook');
+    res.send(`here ist the book ${req.params.id}`);
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
+    res.status(500).send('something went wrong');
   }
 };
