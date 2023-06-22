f1e93f9e63842962a33ebe6739ed1ef7bd798341
elvisbevab <elvis.bevab@mailbox.org>
2023-06-22 13:19:32 +0200

    styling added to navbar

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Navigation() {
   return (
-    <div>
-      Navigation
-      <ul>
-        <li>
-          <a href='/'>Home</a>
-        </li>
-        <li>
-          <a href='/books'>Books</a>
-        </li>
-      </ul>
-    </div>
+    <Navbar bg='dark' variant='dark'>
+      <Container>
+        <Navbar.Brand href='/'>MyLibrary</Navbar.Brand>
+        <Nav className='me-auto'>
+          <Nav.Link href='/'>Home</Nav.Link>
+          <Nav.Link href='/books'>Books</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
 
